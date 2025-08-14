22338fe1a145fdc14e9f86cfc10980e21fa35d7b
muliassa <muliassa@gmail.com>
2025-08-14 20:38:50 +0300

    first compile

--- zcamController.cpp
+++ zcamController.cpp
@@ -617,6 +617,6 @@ using namespace std;
     void ZCAMController::run() {
         while (!stop) {
             singleRun();
-            std::this_thread::sleep_for(chrono::seconds(60));             
+            std::this_thread::sleep_for(std::chrono::seconds(60));             
         }
     }
