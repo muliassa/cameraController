38bb6b490759e78f3f0f40f5da4a7656adc25484
muliassa <muliassa@gmail.com>
2025-08-25 12:49:59 +0300

    result bug

--- someService.cpp
+++ someService.cpp
@@ -133,7 +133,7 @@ void someService::run() {
 
             if (api == "snapshot") {
                 string path = snapshotService->take();
-                json result;
+                auto result = nlohmann::json();
                 result["path"] = path; 
                 post_response(json, "ok", result);
             }
