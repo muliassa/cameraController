26a884d9daae3905c8bf1f838c7ca47585ef2e1e
muliassa <muliassa@gmail.com>
2025-08-25 14:31:07 +0300

    zcam

--- includes/zcam.h
+++ includes/zcam.h
@@ -16,7 +16,7 @@ extern "C" {
 }
 
 using namespace std;
-using namespace nlohmann;
+// using namespace nlohmann;
 
 class ZCAM {
 
@@ -33,7 +33,7 @@ class ZCAM {
 
 public:
 
-    ZCAM(const json& config, const int cam_idx) {
+    ZCAM(const nlohmann::json& config, const int cam_idx) {
     ~ZCAM();
     void initStream();
     void closeStream();
