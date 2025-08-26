947ab256ee217123dd1e9a62a886cf9b573e0348
muliassa <muliassa@gmail.com>
2025-08-26 15:04:16 +0300

    save snap sec

--- zcamSnapshot.cpp
+++ zcamSnapshot.cpp
@@ -22,7 +22,7 @@ string ZCAMSnapshot::take() {
 	auto now = std::chrono::system_clock::now();
     auto time_t = std::chrono::system_clock::to_time_t(now);       
     stringstream ss;
-    ss << root << "zcam/SNAP" << cam_idx << std::put_time(std::localtime(&time_t), "%H%M") << ".JPG";	
+    ss << root << "zcam/SNAP" << cam_idx << std::put_time(std::localtime(&time_t), "%H%M%S") << ".JPG";	
 
     if (zcam->initStream()) {
 	    AVFrame *frame = zcam->getFrame();
